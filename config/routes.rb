e22f794e98b87a3df23bd0e7f39b7a094c1ede9d
Josh Stricklin <jostricklin@gmail.com>
2017-05-12 14:22:31 -0500

    fix routes, create create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'users/new'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
@@ -10,8 +9,8 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
 
   get '/login', to: 'sessions#new'
-  get '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end
