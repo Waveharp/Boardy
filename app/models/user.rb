dff73c5faa8aec94287f4a7bfdf0a06103c27217
Josh Stricklin <jostricklin@gmail.com>
2017-05-18 16:54:05 -0500

    add user edit functin

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, 
    	presence: true,
-   	length: {minimum: 6}
+   	length: {minimum: 6},
+    allow_nil: true
 
   # returns has digest of given string
   def User.digest(string)
